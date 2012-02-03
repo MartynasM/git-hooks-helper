8c082a7b8394f492aeac5b09ee1a39d8d18491da
Martynas <martynas@samesystem.dk>
2012-02-03 19:21:28 +0200

    Dumb tests

--- lib/git-hooks-helper/git.rb
+++ lib/git-hooks-helper/git.rb
@@ -4,7 +4,19 @@ module GitHooksHelper
       FILES_TO_WATCH = /(.+\.(e?rb|task|rake|thor|prawn)|[Rr]akefile|[Tt]horfile)/
 
       def in_index
-        `git diff-index --name-only --cached HEAD`.split("\n").select{ |file| file =~ FILES_TO_WATCH }.map(&:chomp)
+        #`git diff-index --name-only --cached HEAD`.split("\n").select{ |file| file =~ FILES_TO_WATCH }.map(&:chomp)
+        puts FILES_TO_WATCH
+        puts "herp.rb" =~ FILES_TO_WATCH
+        if "herp.rb" =~ FILES_TO_WATCH
+          puts "YES"
+        end
+
+        puts puts `git diff-index --name-only --cached HEAD`.split("\n").size
+        puts `git diff-index --name-only --cached HEAD`.split("\n").each do |file|
+          puts file
+          puts file =~ FILES_TO_WATCH
+        end
+        `git diff --cached --name-only --diff-filter=AM HEAD`.split("\n").select{ |file| file =~ FILES_TO_WATCH }.map(&:chomp)
       end
     end
   end
