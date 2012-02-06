478cd527bd57ff4ae50b47580ae9ebeca4094e0c
Martynas <martynas@samesystem.dk>
2012-02-06 10:19:32 +0200

    no file type check

--- lib/git-hooks-helper/git.rb
+++ lib/git-hooks-helper/git.rb
@@ -5,18 +5,7 @@ module GitHooksHelper
 
       def in_index
         #`git diff-index --name-only --cached HEAD`.split("\n").select{ |file| file =~ FILES_TO_WATCH }.map(&:chomp)
-        puts FILES_TO_WATCH
-        puts "herp.rb" =~ FILES_TO_WATCH
-        if "herp.rb" =~ FILES_TO_WATCH
-          puts "YES"
-        end
-
-        puts puts `git diff-index --name-only --cached HEAD`.split("\n").size
-        puts `git diff-index --name-only --cached HEAD`.split("\n").each do |file|
-          puts file
-          puts file =~ FILES_TO_WATCH
-        end
-        `git diff --cached --name-only --diff-filter=AM HEAD`.split("\n").select{ |file| file =~ FILES_TO_WATCH }.map(&:chomp)
+        `git diff --cached --name-only --diff-filter=AM HEAD`.split("\n").map(&:chomp)
       end
     end
   end
